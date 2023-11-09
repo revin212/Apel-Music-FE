3e4e521098f2be54979bb12cd3c1337a425a05c5
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-09 13:57:14 +0700

    Mulai membuat landing page / home, membuat component HeroSection & BenefitSection

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { HeroSection } from '../../components/Home/HeroSection/HeroSection'
+import { BenefitSection } from '../../components/Home/BenefitSection/BenefitSection'
+import { Box } from '@mui/material'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroSection />
+      {/* Class list */}
+      {/* Category list */}
+      <BenefitSection />
+    </Box>
   )
 }
 
