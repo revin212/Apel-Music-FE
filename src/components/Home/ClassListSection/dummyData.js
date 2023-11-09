e819450051244ae6c9425d303b8c4eb3e2214601
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-09 16:07:13 +0700

    Membuat class list section menggunakan dummy data

---
+++ src/components/Home/ClassListSection/dummyData.js
@@ -0,0 +1,44 @@
+export const dummyData = [
+    {
+        id: 1,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+    {
+        id: 2,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+    {
+        id: 3,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+    {
+        id: 4,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+    {
+        id: 5,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+    {
+        id: 6,
+        name: 'Kursus Drummer Special Coach (Eno Netral)',
+        category: 'Drum',
+        price: 8500000,
+        image: 'https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2012-1.png'
+    },
+]
