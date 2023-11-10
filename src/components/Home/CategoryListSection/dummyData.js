9880142dca5ec66819313708ca0ef9cdfac51df0
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-10 08:52:36 +0700

    migrasi image ke local folder, serta merapihkan margin landing page

--- src/components/Home/CategoryListSection/dummyData.js
+++ src/components/Home/CategoryListSection/dummyData.js
@@ -2,41 +2,41 @@ export const dummyData = [
     {
         id: 1,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 2,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 3,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 4,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 5,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 6,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 7,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
     {
         id: 8,
         category: 'Drum',
-        image: "https://revinramadhan21.sirv.com/Images/Apel%20Music%20Image/Rectangle%2011.png"
+        image: "/images/Rectangle 11.png"
     },
 ]
