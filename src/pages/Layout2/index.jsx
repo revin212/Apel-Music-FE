06d9c06a932cd22c67bebff62ced4f982a673089
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-10 10:55:25 +0700

    Mengimplementasikan 2 layout yaitu yang ada footer dan tidak ada footer

--- src/pages/Layout2/index.jsx
+++ src/pages/Layout2/index.jsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../../components/navbar/Navbar";
-import { Footer } from "../../components/footer/Footer";
 
-
-const Layout = () => {
+const Layout2 = () => {
     return(
         <>
             <Navbar />
             <Outlet />
-            <Footer />
         </>
     )
 }
 
-export default Layout;
+export default Layout2;
