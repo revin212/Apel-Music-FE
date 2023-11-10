521647904950410ee33baf52ace76f97e74efd62
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-10 15:00:17 +0700

    implementasi axios pada landing page menggunakan api endpoint yang disediakan

--- src/components/Home/CategoryListSection/CategoryListSection.jsx
+++ src/components/Home/CategoryListSection/CategoryListSection.jsx
@@ -4,14 +4,14 @@ import { imageStyle, wrapperStyle, titleStyle, categoryNameStyle } from './Categ
 import { dummyData } from './dummyData'
 import { Link } from 'react-router-dom'
 
-export const CategoryListSection = () => {
+export const CategoryListSection = ({categoryData}) => {
   return (
     <Box sx={wrapperStyle}>
         <Typography variant='h2' sx={titleStyle}>
             Pilih kelas impian kamu
         </Typography>
         <Grid container columnSpacing={3} rowSpacing={7.5}>
-            {dummyData.map((item)=>{
+            {/* {dummyData.map((item)=>{
                 return (
                     <Grid xs={6} md={3} key={item.id}>
                         <Link to='/category/drum/' underline='none' style={{textDecoration:'none'}}>
@@ -19,6 +19,20 @@ export const CategoryListSection = () => {
                             <img src={item.image} width="350" height="234" alt={item.category} style={imageStyle} />
                             <Typography variant='h4' sx={categoryNameStyle}>
                                 {item.category}
+                            </Typography>
+                        </Stack>
+                        </Link>
+                    </Grid>
+                )
+            })}  */}
+            {categoryData.map((item)=>{
+                return (
+                    <Grid xs={6} md={3} key={item.id_type}>
+                        <Link to='/category/drum/' underline='none' style={{textDecoration:'none'}}>
+                        <Stack direction='column' gap='24px' alignItems='center'>
+                            <img src='https://placehold.co/600x400' width="350" height="234" alt={item.type_name} style={imageStyle} />
+                            <Typography variant='h4' sx={categoryNameStyle}>
+                                {item.type_name}
                             </Typography>
                         </Stack>
                         </Link>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HeroSection } from '../../components/Home/HeroSection/HeroSection'
 import { BenefitSection } from '../../components/Home/BenefitSection/BenefitSection'
 import { Box } from '@mui/material'
 import { ClassListSection } from '../../components/Home/ClassListSection/ClassListSection'
 import { CategoryListSection } from '../../components/Home/CategoryListSection/CategoryListSection'
+import axios from 'axios'
 
 const Home = () => {
+  const [classData, setClassData] = useState([])
+  const [categoryData, setCategoryData] = useState([])
+
+  useEffect(()=> {
+    axios.get('http://52.237.194.35:2024/api/Menu/GetMenuLimit')
+      .then((response) => {
+        // handle success
+        setClassData(response.data)
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+
+    axios.get('http://52.237.194.35:2024/api/Type/GetActiveType')
+    .then((response) => {
+      // handle success
+      setCategoryData(response.data)
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+  }, [])
+
   return (
     <Box>
       <HeroSection />
-      <ClassListSection />
-      <CategoryListSection />
+      <ClassListSection classData={classData} />
+      <CategoryListSection categoryData={categoryData} />
       <BenefitSection />
     </Box>
   )
