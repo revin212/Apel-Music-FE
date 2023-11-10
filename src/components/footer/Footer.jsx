ca2a61adb8b93f68553cf3a648a6f4164e3ec156
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-11-10 10:54:24 +0700

    Membuat komponen footer dengan stylingnya

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,81 @@
+import { Avatar, Box, Stack, Typography } from '@mui/material'
+import { wrapperStyle, titleStyle, bodyStyle, listWrapperStyle } from './FooterStyles'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+import { Instagram, LocalPhone, MailOutline, Telegram, YouTube } from '@mui/icons-material'
+
+
+export const Footer = () => {
+
+  return (
+    <Box sx={wrapperStyle}>
+      <Stack direction='column' gap='8px'>
+        <Typography variant='h4' sx={titleStyle}>
+          Tentang
+        </Typography>
+        <Typography variant='body1' sx={bodyStyle}>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+        </Typography>
+      </Stack>
+      <Stack direction='column' gap='14px'>
+        <Typography variant='h4' sx={titleStyle}>
+          Produk
+        </Typography>
+        <Stack direction='row' gap='8px' justifyContent={{xs:'center', md:'start'}}>
+          <ul style={listWrapperStyle}>
+            <li><Link to="#" className='link-footer'>Gitar</Link></li>
+            <li><Link to="#" className='link-footer'>Biola</Link></li>
+            <li><Link to="#" className='link-footer'>Drum</Link></li>
+          </ul>
+          <ul style={listWrapperStyle}>
+            <li><Link to="#" className='link-footer'>Menyanyi</Link></li>
+            <li><Link to="#" className='link-footer'>Piano</Link></li>
+            <li><Link to="#" className='link-footer'>Saxophone</Link></li>
+          </ul>
+        </Stack>
+      </Stack>
+      <Stack direction='column' gap='16px'>
+        <Stack direction='column' gap='10px'>
+          <Typography variant='h4' sx={titleStyle}>
+            Alamat
+          </Typography>
+          <Typography variant='body1' sx={bodyStyle}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.
+          </Typography>
+        </Stack>
+        <Stack direction='column' gap='8px'>
+          <Typography variant='h4' sx={titleStyle}>
+            Kontak
+          </Typography>
+          <Stack direction='row' gap='16px' justifyContent={{xs:'center', md:'start'}}>
+            <Link to='#'>
+            <Avatar sx={{backgroundColor:'secondary.main'}} >
+              <LocalPhone color='primary' />
+            </Avatar>
+            </Link>
+            <Link to='#'>
+            <Avatar sx={{backgroundColor:'secondary.main'}} >
+              <Instagram color='primary' />
+            </Avatar>
+            </Link>
+            <Link to='#'>
+            <Avatar sx={{backgroundColor:'secondary.main'}} >
+              <YouTube color='primary' />
+            </Avatar>
+            </Link>
+            <Link to='#'>
+            <Avatar sx={{backgroundColor:'secondary.main'}} >
+              <Telegram color='primary' />
+            </Avatar>
+            </Link>
+            <Link to='#'>
+            <Avatar sx={{backgroundColor:'secondary.main'}} >
+              <MailOutline color='primary' />
+            </Avatar>
+            </Link>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  )
+}
