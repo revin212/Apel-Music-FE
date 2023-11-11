39129c0cdd3fc613b76e4332d7a1510740b1d1e9
Firman Zulkarnain <sco1oter@gmail.com>
2023-11-11 07:22:35 +0700

    Implementasi axios pada categoryclasslist, merapihkan tag img dgn src base64

--- src/components/CategoryClassList/ClassListSection/ClassListSection.jsx
+++ src/components/CategoryClassList/ClassListSection/ClassListSection.jsx
@@ -3,28 +3,47 @@ import Grid from '@mui/material/Unstable_Grid2'
 import { categoryStyle, imageStyle, wrapperStyle, classNameStyle, priceStyle, titleStyle } from './ClassListSectionStyles'
 import { dummyData } from '../../../utils/dummyData'
 import { Link } from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 
-export const ClassListSection = () => {
+export const ClassListSection = ({categoryName}) => {
+    //const { id } = categoryName
+    const [classData, setClassData] = useState([])
+    useEffect(() => {
+        axios.get(`http://52.237.194.35:2024/api/Menu/GetMenuByTypeName?type_name=${categoryName}`)
+        .then((response) => {
+          // handle success
+          //console.log(response.data)
+          setClassData(response.data)
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        })
+    
+      
+    }, [])
+    
   return (
     <Box sx={wrapperStyle}>
         <Typography variant='h2' sx={titleStyle}>
             Kelas yang tersedia
         </Typography>
         <Grid container columnSpacing={3} rowSpacing={7.5}>
-            {dummyData.map((item)=>{
+            {classData.map((item)=>{
                 return (
                     <Grid xs={12} md={4} key={item.id}>
-                        <Link to='/class/1' underline='none' style={{textDecoration:'none'}}>
+                        <Link to={`/class/${item.id_menu}`} underline='none' style={{textDecoration:'none'}}>
                         <Stack direction='column' gap='16px'>
-                            <img src={item.image} width="350" height="234" alt={item.name} style={imageStyle} />
+                            <img src={`data:image/jpeg;base64,${item.image}`} width="350" height="234" alt={item.name} style={imageStyle} />
                             <Box padding='0px, 8px, 0px, 8px'>
                                 <Box minHeight='113px' maxWidth='292px'>
                                     <Typography variant='body1' sx={categoryStyle}>
-                                        {item.category}
+                                        {categoryName}
                                     </Typography>
                                     <Typography variant='h4' sx={classNameStyle}>
-                                        {item.name}
+                                        {item.title}
                                     </Typography>
                                     <Typography variant='subtitle1' sx={priceStyle}>
                                         IDR {new Intl.NumberFormat(["ban", "id"]).format(item.price)}
--- src/components/CategoryClassList/HeroSection/HeroSection.jsx
+++ src/components/CategoryClassList/HeroSection/HeroSection.jsx
@@ -1,16 +1,34 @@
 import { Box, Stack, Typography, Card } from '@mui/material'
 import { bodyStyle, titleStyle, wrapperStyle } from './HeroSectionStyles'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export const HeroSection = () => {
+export const HeroSection = ({categoryName}) => {
+  const [classData, setClassData] = useState([])
+  useEffect(() => {
+    axios.get(`http://52.237.194.35:2024/api/Type/GetTypeByName?name=${categoryName}`)
+    .then((response) => {
+      // handle success
+      //console.log(response.data)
+      setClassData(response.data)
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+
+  
+}, [])
   return (
+    
     <Box sx={wrapperStyle} >
-        <img src="/images/image 3-1.png" alt="drum" style={{width:'100%', height:'auto'}} />
+        <img src={`data:image/jpeg;base64,${classData.image}`} alt="" style={{width:'100%', height:'auto'}} />
         <Stack direction='column' gap='16px' paddingInline={{xs: '30px',sm:'80px',md:'120px'}} paddingBlock='80px'>
             <Typography variant='h4' sx={titleStyle}>
-                Drummer Class
+                {classData.type_name}
             </Typography>
             <Typography variant='body1' sx={bodyStyle}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                {classData.description}
             </Typography>
         </Stack>
     </Box>
