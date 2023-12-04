f89f4ab7a06862dbc895976e02acd62d399c047d
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-12-04 12:37:20 +0700

    Implementasi loading skeleton & error pada my class

--- src/pages/MyClass/MyClass.jsx
+++ src/pages/MyClass/MyClass.jsx
@@ -1,16 +1,29 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Alert, Box, Stack, Typography } from "@mui/material"
 import { classDescStyle, classListWrapperStyle, classWrapperStyle, containerStyle, imgStyle, imgWrapperStyle } from './MyClassStyles'
 import { dummyDataCheckout } from "../../utils/dummyDataCheckout"
 import { dateToString } from "../../utils/DateUtils"
 import { useState } from "react"
+import { SkeletonMyClass } from "../../components/Skeleton/SkeletonMyClass"
 
 export const MyClass = () => {
     const [classList, setClassList] = useState(dummyDataCheckout.slice(0,2))
+    const loading = false
+    const errorState = false
 
   return (
     <Box sx={containerStyle}>
+        {errorState && 
+        <Alert variant="outlined" severity="error" sx={{color:'warning.main', mt:'48px', mx:'32px'}}>
+            Terjadi kesalahan pada server, mohon muat ulang halaman beberapa saat lagi
+        </Alert>}
+        {!errorState && 
         <Box sx={classListWrapperStyle}>
-            {classList.map((item, i)=>{
+            {loading && [...Array(3)].map((item, i)=>{
+                    return(
+                            <SkeletonMyClass key={i} />
+                    )
+                })}
+            {!loading && classList.map((item, i)=>{
                 return(
                 <Stack key={i} direction={{xs:"column", md:"row"}} gap='24px' alignItems='center' sx={classWrapperStyle}>
                     <Stack sx={imgWrapperStyle}>
@@ -21,15 +34,17 @@ export const MyClass = () => {
                             <Typography fontWeight={400} fontSize={16} sx={{color:'text.gray3'}}>{item.type}</Typography>
                             <Typography fontWeight={600} fontSize={24} sx={{color:'text.gray1'}}>{item.title}</Typography>
                             <Typography fontWeight={400} fontSize={16} my='4px' sx={{color:'text.gray2'}}>Jadwal : {dateToString(item.jadwal)}</Typography>
-                            <Typography fontWeight={600} fontSize={20} sx={{color: 'secondary.main'}}>
-                                IDR {new Intl.NumberFormat(["ban", "id"]).format(item.price)}
-                            </Typography>
                         </Box>
                     </Stack>
                 </Stack>
                 )}
             )}
-        </Box>
+            {!loading && classList.length == 0 && 
+                <Typography variant="body" sx={{color:'text.gray2', mt:'48px', mx:'32px'}}>
+                    Kamu belum mengikuti kelas
+                </Typography>
+            }
+        </Box>}
     </Box>
   )
 }
