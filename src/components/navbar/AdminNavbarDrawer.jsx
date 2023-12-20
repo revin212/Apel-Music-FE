d4579490890bd9d42eb24ae0479fe61e0915cfc5
Revin Dennis Ramadhan <revinramadhan21@gmail.com>
2023-12-20 15:13:39 +0700

    fix import path

--- src/components/navbar/AdminNavbarDrawer.jsx
+++ src/components/navbar/AdminNavbarDrawer.jsx
@@ -2,7 +2,7 @@ import { Class, Home, Logout, Payment, Person, Piano, Receipt, ShoppingCart } fr
 import { Drawer, Stack, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
-import { drawerMenuWrapperStyle } from './NavbarStyles';
+import { drawerMenuWrapperStyle } from './navbarStyles';
 import usePostData from '../../hooks/usePostData';
 import { AuthContext } from '../AuthContext/AuthContext';
 
